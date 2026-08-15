dad96d1e80eb2ac3fc37a3d22119a4ddcb325649
dlzada <danielluiz1607@gmail.com>
2026-08-15 08:36:36 -0300

    chore: initial commit

---
+++ src/main/java/com/dlflix/DlflixApplication.java
@@ -0,0 +1,13 @@
+package com.dlflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlflix/DlflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
