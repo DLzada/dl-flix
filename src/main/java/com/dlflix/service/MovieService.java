aa390cfeaa3cc2e650778d8bf20b6f1b2f44e70c
dlzada <danielluiz1607@gmail.com>
2026-08-19 08:26:07 -0300

    feat(MovieService): add methods for find categories and streamings and return in the rescue of movies

--- src/main/java/com/dlflix/service/MovieService.java
+++ src/main/java/com/dlflix/service/MovieService.java
@@ -1,23 +1,43 @@
 package com.dlflix.service;
 
-import com.dlflix.controller.response.MovieResponse;
+import com.dlflix.entity.Category;
 import com.dlflix.entity.Movie;
+import com.dlflix.entity.Streaming;
 import com.dlflix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
     private final MovieRepository movieRepository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public Movie save(Movie movie){
+        movie.setCategories(findCategories(movie.getCategories()));
+        movie.setStreamings(findStreamings(movie.getStreamings()));
         return movieRepository.save(movie);
     }
 
     public List<Movie> findAll(){
         return movieRepository.findAll();
+    }
+
+    public List<Category> findCategories(List<Category> categories){
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category -> categoryService.findById(category.getId()).ifPresent(categoriesFound::add));
+
+        return categoriesFound;
+    }
+
+    public List<Streaming> findStreamings(List<Streaming> streamings){
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(streaming -> streamingService.findById(streaming.getId()).ifPresent(streamingsFound::add));
+
+        return streamingsFound;
     }
 }
