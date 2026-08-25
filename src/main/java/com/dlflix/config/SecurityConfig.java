8f6d1269a521dcdd60025a2d7127b359047928f7
dlzada <danielluiz1607@gmail.com>
2026-08-25 08:21:44 -0300

    feat(security): creates SecurityFilter and JWTUserData, creates method verifyToken in TokenService

--- src/main/java/com/dlflix/config/SecurityConfig.java
+++ src/main/java/com/dlflix/config/SecurityConfig.java
@@ -13,11 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -30,7 +33,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/dlflix/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
-                //.addFilter()
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
